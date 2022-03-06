424bfe90224ceeb928081a9c7277d657c05b65fd
jushinpon <32800219+jushinpon@users.noreply.github.com>
2022-03-06 17:22:37 +0800

    object examples

---
+++ perl_obj/Horse.pm
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+
+package Horse;
+use parent qw(Animal);
+
+sub sound { "neigh" }
+
+1;
---
+++ perl_obj/speak.pl
@@ -0,0 +1,6 @@
+use strict;
+use warnings;
+use lib ".";
+use Horse;
+
+Horse->speak();
